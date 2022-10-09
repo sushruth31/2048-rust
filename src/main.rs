dfe405f73176330f44c7fae9185d662151689252
Sushruth Chandrasekar <sushruth31@gmail.com>
2022-10-09 00:43:48 -0400

    i am a god

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,218 @@
-use gloo::console::log;
+use gloo::{console::log, events::EventListener};
 use rand::Rng;
+use wasm_bindgen::JsCast;
 use yew::prelude::*;
 
 const GRID_SIZE: i32 = 4;
 
+type BoardType = Vec<Vec<i32>>;
+
+fn shift_board_right(grid: &mut BoardType) -> BoardType {
+    for (i, row) in grid.iter_mut().enumerate() {
+        let mut count = 0;
+        while count < 5 {
+            count += 1;
+            for j in 0..GRID_SIZE - 1 {
+                let curr = row[j as usize];
+                let next_j = (j + 1) as usize;
+                let next = row[next_j];
+                if next == 0 {
+                    //shift right
+                    row[next_j] = curr;
+                    row[j as usize] = 0;
+                }
+                if curr == next {
+                    row[next_j] = next * 2;
+                    row[j as usize] = 0;
+                }
+            }
+        }
+    }
+    grid.to_owned()
+}
+
+fn shift_board_left(grid: &mut BoardType) -> BoardType {
+    for (i, row) in grid.iter_mut().enumerate() {
+        let mut count = 0;
+        while count < 5 {
+            count += 1;
+            for j in 0..GRID_SIZE - 1 {
+                let curr = row[j as usize];
+                let next_i = (j + 1) as usize;
+                let next = row[next_i];
+                if curr == 0 {
+                    //move next left;
+                    row[j as usize] = next;
+                    row[next_i] = 0;
+                }
+                if curr == next {
+                    //add if numbers are the same
+                    row[j as usize] = curr * 2;
+                    row[next_i] = 0;
+                }
+            }
+        }
+    }
+    grid.to_owned()
+}
+
+fn rotate_grid_left(grid: &mut BoardType) -> BoardType {
+    let mut target = build_grid(GRID_SIZE);
+    for (i, row) in grid.iter().enumerate() {
+        for (j, num) in row.iter().enumerate() {
+            //grid row should become target col
+            //grid col should become target row but its gridsize -1 - col
+            let targetrow = GRID_SIZE - 1 - (j as i32);
+            target[targetrow as usize][i] = grid[i][j]
+        }
+    }
+    target
+}
+
+fn rotate_grid_right(grid: &mut BoardType) -> BoardType {
+    let mut target = build_grid(GRID_SIZE);
+    for (i, row) in grid.iter().enumerate() {
+        for (j, num) in row.iter().enumerate() {
+            //grid row should be target col but Gridsize - it
+            //grid col should become target row but its gridsize -1 - col
+            let targetcol = GRID_SIZE - 1 - (i as i32);
+            target[j][targetcol as usize] = grid[i][j];
+        }
+    }
+    target
+}
+
+pub struct Board {
+    board: BoardType,
+}
+
+impl Board {
+    fn rotate_left(&mut self) -> Board {
+        let mut target = build_grid(GRID_SIZE);
+        for (i, row) in self.board.iter().enumerate() {
+            for (j, num) in row.iter().enumerate() {
+                //grid row should become target col
+                //grid col should become target row but its gridsize -1 - col
+                let targetrow = GRID_SIZE - 1 - (j as i32);
+                target[targetrow as usize][i] = self.board[i][j]
+            }
+        }
+        Board { board: target }
+    }
+
+    fn shift_up(&mut self) -> Board {
+        //rotate left then shift left the rotate right
+        self.rotate_left().shift_board_left().rotate_right()
+    }
+
+    fn rotate_right(&mut self) -> Board {
+        let mut target = build_grid(GRID_SIZE);
+        for (i, row) in self.board.iter().enumerate() {
+            for (j, num) in row.iter().enumerate() {
+                //grid row should be target col but Gridsize - it
+                //grid col should become target row but its gridsize -1 - col
+                let targetcol = GRID_SIZE - 1 - (i as i32);
+                target[j][targetcol as usize] = self.board[i][j];
+            }
+        }
+        Board { board: target }
+    }
+
+    fn shift_board_left(&mut self) -> Board {
+        for (i, row) in self.board.iter_mut().enumerate() {
+            let mut count = 0;
+            while count < 5 {
+                count += 1;
+                for j in 0..GRID_SIZE - 1 {
+                    let curr = row[j as usize];
+                    let next_i = (j + 1) as usize;
+                    let next = row[next_i];
+                    if curr == 0 {
+                        //move next left;
+                        row[j as usize] = next;
+                        row[next_i] = 0;
+                    }
+                    if curr == next {
+                        //add if numbers are the same
+                        row[j as usize] = curr * 2;
+                        row[next_i] = 0;
+                    }
+                }
+            }
+        }
+        Board {
+            board: self.board.to_owned(),
+        }
+    }
+
+    fn shift_board_right(&mut self) -> Board {
+        for (i, row) in self.board.iter_mut().enumerate() {
+            let mut count = 0;
+            while count < 5 {
+                count += 1;
+                for j in 0..GRID_SIZE - 1 {
+                    let curr = row[j as usize];
+                    let next_j = (j + 1) as usize;
+                    let next = row[next_j];
+                    if next == 0 {
+                        //shift right
+                        row[next_j] = curr;
+                        row[j as usize] = 0;
+                    }
+                    if curr == next {
+                        row[next_j] = next * 2;
+                        row[j as usize] = 0;
+                    }
+                }
+            }
+        }
+        Board {
+            board: self.board.to_owned(),
+        }
+    }
+
+    fn shift_down(&mut self) -> Board {
+        //rotate right shift lef rotate left
+        self.rotate_right().shift_board_left().rotate_left()
+    }
+}
+
 #[function_component]
 fn App() -> Html {
-    let grid: UseStateHandle<Vec<Vec<i32>>> = use_state(|| build_grid(GRID_SIZE));
+    let grid: UseStateHandle<BoardType> = use_state(|| build_grid(GRID_SIZE));
+    {
+        let grid = grid.clone();
+        let grid_inner = grid.clone();
+        let mut board = Board {
+            board: grid.to_vec(),
+        };
+        use_effect_with_deps(
+            move |_| {
+                let document = gloo::utils::document();
+                let listener = EventListener::new(&document, "keydown", move |evt| {
+                    let e = evt.dyn_ref::<web_sys::KeyboardEvent>().unwrap();
+                    let key = e.key();
+                    match key.as_str() {
+                        "ArrowRight" => {
+                            grid_inner.set(board.shift_board_right().board);
+                        }
+                        "ArrowLeft" => {
+                            grid_inner.set(board.shift_board_left().board);
+                        }
+                        "ArrowUp" => {
+                            grid_inner.set(board.shift_up().board);
+                        }
+                        "ArrowDown" => {
+                            grid_inner.set(board.shift_down().board);
+                        }
+                        _ => (),
+                    }
+                });
+                || drop(listener)
+            },
+            grid,
+        );
+    }
     let rows = grid.to_vec().into_iter().map(|row| {
         let cols = row.into_iter().map(|num| {
             html! {
